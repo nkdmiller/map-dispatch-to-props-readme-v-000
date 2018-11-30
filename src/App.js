49251aa733f1b28392f720f56764ab1f569da984
Nate Miller <natekmiller96@gmail.com>
2018-11-30 04:39:46 +0000

    Automatically backed up by Learn

--- src/App.js
+++ src/App.js
@@ -26,5 +26,11 @@ const mapStateToProps = (state) => {
     items: state.items
   };
 };
-
+const mapDispatchToProps = dispatch => {
+  return {
+    addItem: () => {
+      dispatch(addItem())
+    }
+  };
+};
 export default connect(mapStateToProps)(App);
